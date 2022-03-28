ffdb0c901902e79a39ba17befb398a77858bc5ce
Aleksandra Gorbule <gorbule2@inbox.lv>
2022-03-28 14:28:36 +0300

    Spring MVC demo project. Udemy course.

---
+++ src/main/java/com/luv2code/springdemo/springMVCdemo/SpringMvcDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo.springMVCdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luv2code/springdemo/springMVCdemo/SpringMvcDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo.springMVCdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
