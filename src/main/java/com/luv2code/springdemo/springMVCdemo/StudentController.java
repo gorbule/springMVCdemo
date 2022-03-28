8a8a2193cd7e3953d79f4ace5fbfc56e51f1e557
Aleksandra Gorbule <gorbule2@inbox.lv>
2022-03-28 17:12:24 +0300

    Spring MVC Form tags. Student Form

--- src/main/java/com/luv2code/springdemo/springMVCdemo/StudentController.java
+++ src/main/java/com/luv2code/springdemo/springMVCdemo/StudentController.java
@@ -1,4 +1,31 @@
 package com.luv2code.springdemo.springMVCdemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
+
+    //create method to show form
+    @RequestMapping("/showForm")
+    public String showForm(Model theModel) {
+        //create a new student object
+        Student theStudent = new Student();
+
+        //add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "studentForm";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        //log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        return "student-confirmation";
+
+    }
 }
