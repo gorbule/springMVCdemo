da12c0c037fceff3c5207bd0fb1ea881e7be396c
Aleksandra Gorbule <gorbule2@inbox.lv>
2022-03-29 14:29:56 +0300

    Spring MVC Form tags. Student Form

--- src/main/java/com/luv2code/springdemo/springMVCdemo/Student.java
+++ src/main/java/com/luv2code/springdemo/springMVCdemo/Student.java
@@ -1,12 +1,29 @@
 package com.luv2code.springdemo.springMVCdemo;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    //let's add an Array of Strings
+    private String[] operatingSystems;
+
+    private LinkedHashMap<String, String> countryOptions;
 
     //non arg constructor
     public Student() {
+        //populate country options: used ISO country code
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("LV", "Latvia");
+
     }
 
     //getters and setters
@@ -25,4 +42,35 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getCountry() {
+        return country;
+    }
+//set method will be realized after Submit button pressing
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String[] getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public void setOperatingSystems(String[] operatingSystems) {
+        this.operatingSystems = operatingSystems;
+    }
+
+    //when form is loaded - Spring will call student.getCountryOptions()
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
+
+
 }
